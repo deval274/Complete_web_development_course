fd33a615e21c2bed41fb0af1bdf5d45dbc2a6e67
Deval_Ujeniya <devalujeniya@gmail.com>
2026-08-17 11:50:36 +0530

    Project(project-management): Add dotenv and nodemon for local development.

--- projects/project-management/index.js
+++ projects/project-management/index.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const username = process.env.username;
+
+console.log("Hello Worldddd", username);
