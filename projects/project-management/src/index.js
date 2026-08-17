eebcedf8e78912a72fb661ed6b638d1671e13512
Deval_Ujeniya <devalujeniya@gmail.com>
2026-08-17 19:17:26 +0530

    Project(project-management): Refactor server setup by creating app.js and updating index.js

--- projects/project-management/src/index.js
+++ projects/project-management/src/index.js
@@ -1,17 +1,11 @@
 import dotenv from "dotenv";
-import express from "express";
+import app from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-const app = express();
-
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 app.listen(PORT, () => {
   console.log("server is running on the port ", PORT);
