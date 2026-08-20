c51a204d950cf1005d3ee1dbf5fad68aac523940
Deval_Ujeniya <devalujeniya@gmail.com>
2026-08-20 17:10:53 +0530

    Project(project-management): Integrate MongoDB with Mongoose and refactor server startup

--- projects/project-management/src/index.js
+++ projects/project-management/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./db/index.js";
 
 dotenv.config({
   path: "./.env",
@@ -7,6 +8,13 @@ dotenv.config({
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log("server is running on the port ", PORT);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("server is running on the port ", PORT);
+    });
+  })
+  .catch((error) => {
+    console.error("Mongodb connection error:", error);
+    process.exit(1);
+  });
