8ba64f9a523eb7239cb85fa1df9afee247e1b416
Deval_Ujeniya <devalujeniya@gmail.com>
2026-08-20 16:13:03 +0530

    Project(project-management): Express configuration and CORS

--- projects/project-management/src/app.js
+++ projects/project-management/src/app.js
@@ -1,6 +1,20 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true, // allow cookies to be sent from the client to the server
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
