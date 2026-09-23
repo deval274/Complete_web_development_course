b2a9043c9e0ca2074d72a6bd5f2f6e9c340c6a56
Deval_Ujeniya <devalujeniya@gmail.com>
2026-09-23 14:55:26 +0530

    Project(project-management): Implement user registration and email verification functionality

---
+++ projects/project-management/src/controllers/auth.contollers.js
@@ -0,0 +1,76 @@
+import { ApiResponse } from "../utils/api.response.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import { User } from "../models/user.models.js";
+import { sendEmail } from "../utils/mail.js";
+
+const generateAccessAndRefreshTokens = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      throw new ApiError(404, "User not found", []);
+    }
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(500, "Internal server error", [error.message]);
+  }
+};
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { email, username, password, role } = req.body;
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existedUser) {
+    throw new ApiError(400, "User already exists", []);
+  }
+
+  const user = await User.create({
+    email,
+    username,
+    password,
+    isEmailVerified: false,
+  });
+
+  const { unHashedToken, hashedToken, tokenExpiry } =
+    await generateTemporaryToken();
+
+  user.emailVerificationToken = hashedToken;
+  user.emailVerificationTokenExpiry = tokenExpiry;
+
+  await user.save({ validateBeforeSave: false });
+
+  await sendEmail({
+    email: user.email,
+    subject: "Email Verification",
+    mailgenContent: emailVerificationMailgenContent(
+      user.username,
+      `${req.protocol}://${req.get("host")}/api/v1/users/verify-email/${unHashedToken}`,
+    ),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken -emailVerificationToken -emailVerificationTokenExpiry",
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "Internal server error", []);
+  }
+
+  return res
+    .status(201)
+    .json(
+      new ApiResponse(
+        201,
+        { user: createdUser },
+        "User created successfully and verification email sent",
+      ),
+    );
+});
+
+export { registerUser };
