a5b0b6fd73c5dc5b00ec9abb8da00edb0720c5c3
Deval_Ujeniya <devalujeniya@gmail.com>
2026-09-24 16:29:29 +0530

    Project(project-management): Implement logout functionality and update authentication routes

--- projects/project-management/src/routes/auth.routes.js
+++ projects/project-management/src/routes/auth.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/auth.contollers.js";
+import {
+  loginUser,
+  logoutUser,
+  registerUser,
+} from "../controllers/auth.contollers.js";
 import { validation } from "../middlewares/validator.middleware.js";
 import {
   userRegisterValidator,
   userLoginValidator,
 } from "../validators/index.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
@@ -12,5 +17,6 @@ router
   .route("/register")
   .post(userRegisterValidator(), validation, registerUser);
 router.route("/login").post(userLoginValidator(), validation, loginUser);
+router.route("/logout").post(verifyJwt, logoutUser);
 
 export default router;
